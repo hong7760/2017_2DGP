df33783f5551d857d5437fd307689053865ba45f
hong7760 <qkfand2@naver.com>
2017-11-22 18:40:41 +0900

    TILE_SIZE, MAP_SIZE 추가

--- Labs/Jungle_A_Z/forest.py
+++ Labs/Jungle_A_Z/forest.py
@@ -3,7 +3,8 @@ from pico2d import *
 class Forest:
 
     image = None
-
+    TILE_SIZE = 48
+    MAP_SIZE = 1920
     def __init__(self):
         if Forest.image == None:
             Forest.image = load_image('./resource/Map.png')
@@ -13,8 +14,6 @@ class Forest:
     def update(self, x, y):
         self.x = int(x)
         self.y = int(y)
-        #print(*self.collision_tile(self.mouse_x, self.mouse_y))
-        print(self.mouse_x, self.mouse_y)
 
     def draw(self):
         self.image.clip_draw(self.x - 400, self.y - 300, 800,600,400,300)
@@ -26,13 +25,13 @@ class Forest:
 
     def collision_tile(self, x,y):
 
-        for i in range(424, 2320, 48):
-            for j in range(324, 2220, 48):
+        for i in range(400 + int(self.TILE_SIZE/2) , 400 + self.MAP_SIZE, self.TILE_SIZE):
+            for j in range(300 + int(self.TILE_SIZE/2), 300 + self.MAP_SIZE, self.TILE_SIZE):
                 left_b, bottom_b, right_b, top_b = i - 24, j - 24, i + 24, j + 24
-                if x < right_b and x > left_b and y > bottom_b and y < top_b:
+                if x <= right_b and x >= left_b and y >= bottom_b and y <= top_b:
                     return i - self.x, j - self.y
 
         return 0, 0
 
     def draw_tile_rect(self, x, y):
-        draw_rectangle(x - 24 , y - 24, x + 24, y + 24)
+        draw_rectangle(x - self.TILE_SIZE/2 , y - self.TILE_SIZE/2, x + self.TILE_SIZE/2, y + self.TILE_SIZE/2)
--- Labs/Jungle_A_Z/start_state.py
+++ Labs/Jungle_A_Z/start_state.py
@@ -10,7 +10,7 @@ logo_time = 0.0
 def enter():
     global image
     open_canvas()
-    image = load_image('kpu_credit.png')
+    image = load_image('./resource/kpu_credit.png')
 
 
 def exit():
